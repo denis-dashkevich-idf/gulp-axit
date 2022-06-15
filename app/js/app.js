89f4edde3a8e8bc81bc4f2d363793e2ac7659d5c
denis-dashkevich-idf <denis.dashkevich@moneyman.ru>
2022-06-15 20:39:53 +0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,13 @@
+// // Import jQuery module (npm i jquery)
+// import $ from 'jquery'
+// window.jQuery = $
+// window.$ = $
+import Splide from '@splidejs/splide';
+// // Import vendor jQuery plugin example (not module)
+// require('~/app/libs/mmenu/dist/mmenu.js')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Custom JS
+
+})
